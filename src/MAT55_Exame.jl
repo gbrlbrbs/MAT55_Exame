e883483808c3f9eb335d575246e78a8801a224fd
Gabriel Martinz <gabrielbarbosamartinz_@outlook.com>
2021-12-04 19:41:34 -0300

    parser for wikipedia page done

--- src/MAT55_Exame.jl
+++ src/MAT55_Exame.jl
@@ -1,5 +1,6 @@
 module MAT55_Exame
-using DataStructures : OrderedDict
+
+using DataStructures: OrderedDict
 import TimeSeries as TS
 import MarketData as MD
 import DataFrames as DF
@@ -7,6 +8,8 @@ import Dates as DT
 import HTTP
 import Gumbo
 using Cascadia
+
+export scrape_wikipedia_table, get_data
 
 const wikipedia_sp500_link = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 
@@ -25,47 +28,40 @@ function get_data(ticker::String)::DF.DataFrame
     return df
 end
 
-function scrape_wikipedia_tables()::Vector{DF.DataFrame}
+function scrape_wikipedia_table()::DF.DataFrame
     sp500_page = HTTP.get(wikipedia_sp500_link)::HTTP.Response
     parsed_page = Gumbo.parsehtml(String(sp500_page.body))::Gumbo.HTMLDocument
-    matches = eachmatch(sel"table", parsed_page.root)::Vector{Gumbo.HTMLNode}
-    # cada match: um DataFrame
-    tables = DF.DataFrame[]
-    for m in matches
-        # pegar os nomes das colunas
-        colnames = String[]
-        # como se fosse um DataFrame
-        # chave: nome da coluna
-        # valor: valores nas colunas
-        dict_table = OrderedDict{String, Vector{String}}()
-        # parse das linhas:
-        rows = eachmatch(sel"tr", m)::Vector{Gumbo.HTMLNode}
-        for (i, row) in enumerate(rows)
-            if (i == 1) # header
-                header = eachmatch(sel"th", row)::Vector{Gumbo.HTMLNode}
-                for col in header
-                    col_name = strip(nodeText(col))::String
-                    test_colname = col_name
-                    k = 2
-                    while (test_colname in colnames)
-                        test_colname = col_name + "_$k"
-                        k += 1
-                    end
-                    push!(colnames, test_colname)
-                end
-            else
-                row_vals = eachmatch(sel"th", row)::Vector{Gumbo.HTMLNode}
-                for (j, node) in enumerate(row_vals)
-                    node_text = strip(nodeText(node))::String
-                    col = colnames[j]
-                    push!(dict_table[col], node_text)
-                end
+    # selecionar os constiuintes do s&p 500
+    match = eachmatch(sel"#constituents", parsed_page.root)::Vector{Gumbo.HTMLNode}
+    # pegar os nomes das colunas
+    colnames = String[]
+    # dict como se fosse um DataFrame
+    # chave: nome da coluna
+    # valor: valores nas colunas
+    dict_table = OrderedDict{String, Vector{String}}()
+    # parse das linhas:
+    rows = eachmatch(sel"tr", match[1])::Vector{Gumbo.HTMLNode}
+    for (i, row) in enumerate(rows)
+        if (i == 1) # header
+            header = eachmatch(sel"th", row)::Vector{Gumbo.HTMLNode}
+            for col in header
+                colname = strip(nodeText(col))::SubString{String}
+                push!(colnames, colname)
+            end
+            for name in colnames
+                dict_table[name] = Vector{String}()
+            end
+        else
+            row_vals = eachmatch(sel"td", row)::Vector{Gumbo.HTMLNode}
+            for (j, node) in enumerate(row_vals)
+                node_text = strip(nodeText(node))::SubString{String}
+                col = colnames[j]
+                push!(dict_table[col], node_text)
             end
         end
-        dataframe = DF.DataFrame(dict_table)
-        push!(tables, dataframe)
     end
-    tables
+    dataframe = DF.DataFrame(dict_table)
+    dataframe
 end
 
 # function get_tickers(start_date::DT.Date)
